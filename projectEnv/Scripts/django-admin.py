bd912acd6b672b4e5bd0fb368721a7fe7b431a05
knkemree <konakziyaemre@gmail.com>
2020-04-19 20:35:00 -0400

    Initial commit

---
+++ projectEnv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\prime\onedrive\desktop\django\zypmedia\projectenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
